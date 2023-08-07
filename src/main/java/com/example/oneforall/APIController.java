0d74401f5467b967fc22183a0e7d9c29346d67f3
Bishesh99 <bisheshsingh.99@gmail.com>
2023-08-07 22:58:50 +0530

    Service created successfully

--- src/main/java/com/example/oneforall/APIController.java
+++ src/main/java/com/example/oneforall/APIController.java
@@ -2,12 +2,13 @@ package com.example.oneforall;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 @RestController
 public class APIController {
     @GetMapping(path="/calculate/{x}/{y}")
     public int calculate(@PathVariable int x, @PathVariable int y) {
-        return new Addition().result(Arrays.asList(x,y));
+        return new Addition().result(new ArrayList<>(Arrays.asList(x,y)));
     }
 }
